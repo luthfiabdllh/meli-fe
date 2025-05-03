989ebc6b96fee45c70a05f40618ddeee61fff018
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-05-03 21:07:08 +0700

    Feat: add App Layout

--- src/components/components/appLayout.tsx
+++ src/components/components/appLayout.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from "react"
+import Header from "./header"
+import Sidebar from "./sidebar"
 
 interface LayoutProps {
   children: ReactNode
@@ -6,17 +8,28 @@ interface LayoutProps {
 
 export default function AppLayout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen bg-slate-50 pb-16 md:pb-0">
-      {/* <Header /> */}
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-12 gap-4 px-4 py-4">
-        <div className="hidden md:block md:col-span-3 lg:col-span-2">
+    <div className="min-h-screen bg-slate-50">
+      {/* Fixed Header */}
+      <Header />
+
+      <div className="flex">
+        {/* Left Sidebar - Fixed on desktop, hidden on mobile */}
+        <aside className="hidden md:block w-64 lg:w-72 fixed left-0 top-16 bottom-0 bg-white border-r overflow-y-auto">
           {/* <LeftSidebar /> */}
-        </div>
-        <main className="md:col-span-9 lg:col-span-7">{children}</main>
-        <div className="hidden lg:block lg:col-span-3">
-          {/* <RightSidebar /> */}
-        </div>
+        </aside>
+
+        {/* Main Content - Adjusts based on sidebar visibility */}
+        <main className="flex-1 min-h-[calc(100vh-64px)] md:ml-64 lg:ml-72 lg:mr-80 p-4">
+          <div className="max-w-2xl mx-auto">{children}</div>
+        </main>
+
+        {/* Right Sidebar - Fixed on large screens, hidden on smaller screens */}
+        <aside className="hidden lg:block w-80 fixed right-0 top-16 bottom-0 bg-white border-l overflow-y-auto">
+          {/* <div className="p-4">{rightSidebarContent || <DefaultRightSidebar />}</div> */}
+        </aside>
       </div>
+
+      {/* Mobile Bottom Bar - Only visible on mobile */}
       <div className="md:hidden">
         {/* <BottomBar /> */}
       </div>
