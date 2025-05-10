86bc03a89ca205ca1ef5db8dacb3a9959245e1f7
luthfiabdllh <ahmadabdllh000@gmail.com>
2025-05-10 11:49:22 +0700

    feat: add create Thread API

---
+++ src/app/api/thread/createThread.ts
@@ -0,0 +1,23 @@
+import instance from "@/lib/axios";
+
+export async function uploadImage(token: string, file: File) {
+  const formData = new FormData();
+  formData.append("image", file);
+
+  const res = await instance.post("/images", formData, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "multipart/form-data",
+    },
+  });
+  return res.data.image_id;
+}
+
+export async function createThread(token: string, data: any) {
+  const res = await instance.post(`/threads`, data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return res.data.userDetailsEdited;
+}
